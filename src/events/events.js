998100893d7efe83e7b546abb7ebb128561e3369
aptivator <aptivator@gmail.com>
2017-01-28 23:22:07 -0500

    Fix minor spacing

--- src/events/events.js
+++ src/events/events.js
@@ -1,7 +1,7 @@
-import _                    from 'lodash';
-import aptivator            from '../lib/instance';
-import on                   from './on/on';
-import off                  from './off/off';
-import trigger              from './trigger/trigger';
+import _         from 'lodash';
+import aptivator from '../lib/instance';
+import on        from './on/on';
+import off       from './off/off';
+import trigger   from './trigger/trigger';
 
 _.extend(aptivator, {on, off, trigger});
