becc30cbeef5cec7fede69078973d8950188c432
aptivator <aptivator@gmail.com>
2017-03-14 21:19:15 -0400

    Rewrite rollup.config and introduce jsnext:main build

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,19 @@ import babel    from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve  from 'rollup-plugin-node-resolve';
 
+let packageJson = require('./package.json');
+let {'jsnext:main': jsnext, main} = packageJson;
+
 export default {
   moduleName: 'aptivator',
   entry: 'src/aptivator.js',
-  dest: 'dist/aptivator.js',
-  format: 'umd',
+  targets: [{
+    format: 'umd',
+    dest: main
+  }, {
+    format: 'es',
+    dest: jsnext
+  }],
   globals: {
     backbone: 'Backbone',
     'backbone.marionette': 'Marionette',
